16933bdde26184306ead04fd7ae003315878d2a0
Jon Baldwin <almarezjbm@gmail.com>
2022-04-08 21:49:22 +0800

    Java Calculator

---
+++ src/mp4_almarez_jonbaldwin/MP4_Almarez_JonBaldwin.java
@@ -0,0 +1,17 @@
+package mp4_almarez_jonbaldwin;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MP4_Almarez_JonBaldwin extends Calculator{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculator Calc = new Calculator();
+    }
+    
+}
